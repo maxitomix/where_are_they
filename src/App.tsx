4b62ce07cf7fdfbb84fcea152d2ee9f95a4a94f1
MaxitoMix <128189550+MaxitoMix@users.noreply.github.com>
2023-07-14 21:48:39 -0400

    progress

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,9 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -30,6 +33,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
     </>
   )
 }
