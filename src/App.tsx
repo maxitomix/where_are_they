f25a2fbda0f3992c29089de30d0e2ec393910a8f
MaxitoMix <128189550+MaxitoMix@users.noreply.github.com>
2023-07-17 17:06:07 -0400

    progress. Background responsive and scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,9 @@ function App() {
   console.log(app)
 
   return (
-    <div className="w-screen h-screen overflow-auto">
-      <div className='shadow-2xl border-8' style={{ width: '7956px', height: '7956px' }}>
-        <div className="w-full h-full" style={{ backgroundImage: `url(${mainImage})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }} />
+    <div className="w-screen h-screen overflow-auto ">
+      <div className='border-8 scrollbar-hide' style={{ width: '3978px', height: '3978px', backgroundImage: `url(${mainImage})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}>
+        
       </div>
     </div>
   );
