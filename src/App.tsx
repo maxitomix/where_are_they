2f2c19d46c0e5d40ee5c8b1934b0d96c474c497b
MaxitoMix <128189550+MaxitoMix@users.noreply.github.com>
2023-07-17 16:48:09 -0400

    progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import {app} from "./services/firebase";
+import mainImage from "./assets/characters.jpg"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
   console.log(app)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-screen h-screen overflow-auto">
+      <div className='shadow-2xl border-8' style={{ width: '7956px', height: '7956px' }}>
+        <div className="w-full h-full" style={{ backgroundImage: `url(${mainImage})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }} />
       </div>
-      <h1>Vite + React</h1>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
