47e1208f2d8c234e76db99eea903a8285963976e
MaxitoMix <128189550+MaxitoMix@users.noreply.github.com>
2023-07-18 16:25:36 -0400

    nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,5 @@
 import searchIcon from '../assets/search.png';
+import userIcon from '../assets/user.png';
 
 export default function NavBar() {
 
@@ -7,8 +8,14 @@ export default function NavBar() {
   return (
     <>
     <div className='flex justify-stretch'>
-      <img className='ml-4 mu-2' src={searchIcon} alt="searchIcon" />
-        <h1 className='mx-auto place-self-center  text-pink-500 font text-3xl font-black bg-pink-100 bg-opacity-75 px-5 py-1 rounded-full'>WHERE ARE THEY??</h1>
+    <div className="flex">
+      <img className='h-12 sm:h-16 place-self-center' src={searchIcon} alt="searchIcon" />
+      </div>
+        <h1 className='mx-auto place-self-center  text-pink-500 font text-sm sm:text-3xl font-black bg-pink-100 bg-opacity-75 px-5 py-1 rounded-full shadow-lg'>WHERE ARE THEY??</h1>
+        <div className="flex">
+        <img className='object-scale-down h-12 sm:h-16 place-self-center' src={userIcon} alt="searchIcon" />
+        </div>
+        
     </div>
     </>
   );
