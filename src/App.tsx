cf356292028649c3882131a2e9e8545a59fd228e
MaxitoMix <128189550+MaxitoMix@users.noreply.github.com>
2023-07-18 12:17:10 -0400

    scrolling window over background

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import './App.css'
 import {app} from "./services/firebase";
 import mainImage from "./assets/characters.jpg"
+import Overlay from './components/Overlay';
 
 
 function App() {
@@ -9,11 +10,18 @@ function App() {
   console.log(app)
 
   return (
-    <div className="w-screen h-screen overflow-auto ">
-      <div className='border-8 scrollbar-hide' style={{ width: '1989px', height: '1989px', backgroundImage: `url(${mainImage})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}>
-        
-      </div>
-    </div>
+
+<>
+
+ <div className='w-screen h-screen overflow-auto border-8  border-red-500'>
+  
+          
+   <div className='border-8 scrollbar-hide ' style={{ width: '1989px', height: '1989px', backgroundImage: `url(${mainImage})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}>
+    
+  </div>
+      
+  </div>
+  </>
   );
 }
 
---
+++ src/components/Overlay.tsx
@@ -0,0 +1,15 @@
+import NavBar from "./navBar";
+
+
+export default function Overlay() {
+
+
+ 
+  return (
+
+    <div className='backdrop-blur-sm'>
+      <NavBar/>
+    </div>
+
+  );
+}
