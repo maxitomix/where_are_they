f4da3391eb159c7e4e1c51174417c598acd06dd4
MaxitoMix <128189550+MaxitoMix@users.noreply.github.com>
2023-07-19 09:48:47 -0400

    prgress

---
+++ src/components/Highscores.tsx
@@ -0,0 +1,28 @@
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons'
+
+export default function HighScores() {
+
+
+ 
+  return (
+    <>
+	<div className="flex flex-col justify-center items-center h-screen">
+    <div className="container mx-auto px4">
+      
+    </div>
+		
+		<button className='h-36 w-60 bg-green-200 rounded'>
+      <FontAwesomeIcon icon={faCirclePlay} size="5x" className='text-blue-500'/>
+    </button>
+
+    <div className="container mx-auto px4">
+      
+      </div>
+
+	</div>
+    </>
+  );
+}
+
