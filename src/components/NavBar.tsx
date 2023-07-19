ba25afcc02c2ae62b6d4f498fa4dfb8904c80295
MaxitoMix <128189550+MaxitoMix@users.noreply.github.com>
2023-07-19 13:08:52 -0400

    progress. needs roster

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,19 +1,23 @@
 import searchIcon from '../assets/search.png';
 import userIcon from '../assets/user.png';
 
-export default function NavBar() {
+export default function NavBar({onLogoClick, isPlaying}) {
 
 
  
   return (
     <>
     <div className='flex justify-stretch'>
-    <div className="flex">
-      <img className='h-12 sm:h-16 place-self-center' src={searchIcon} alt="searchIcon" />
+      <div className="flex" >
+        <img className='h-12 sm:h-16 place-self-center' src={searchIcon} alt="searchIcon" onClick={onLogoClick}/>
       </div>
-        <h1 className='mx-auto place-self-center  text-pink-500 font text-sm sm:text-3xl font-black bg-pink-100 bg-opacity-75 px-5 py-1 rounded-full shadow-lg'>WHERE ARE THEY??</h1>
-        <div className="flex">
-        <img className='object-scale-down h-12 sm:h-16 place-self-center' src={userIcon} alt="searchIcon" />
+
+        <h1 className='mx-auto place-self-center  text-pink-500 font text-sm sm:text-3xl font-black bg-pink-100 bg-opacity-75 px-5 py-1 rounded-full shadow-lg'
+        >WHERE ARE THEY??</h1>
+
+      <div className="flex">
+        <img className={`object-scale-down h-12 sm:h-16 place-self-center ${isPlaying ? 'hidden' : ''}`} src={userIcon} alt="searchIcon" />
+        <Roster className={`${isPlaying ? 'hidden' : ''}`}/>
       </div>
         
     </div>
--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,19 +1,30 @@
 import HighScores from "./Highscores";
 import NavBar from "./NavBar";
-import PlayGame from "./PlayGame";
+import PlayGameButton from "./PlayGameButton";
+import { useState } from "react";
 
 
 
 export default function Overlay() {
 
+  // Create a piece of state to track whether the game is being played
+  const [isPlaying, setIsPlaying] = useState(false);
 
+  // Function to toggle isPlaying state
+  const toggleIsPlaying = () => {
+    setIsPlaying(!isPlaying);
+  }
+
+  const restartGame = () => {
+    setIsPlaying(false);
+  }
  
   return (
 
-    <div className='flex flex-col w-screen h-screen backdrop-blur-sm fixed top-0 left-0'>
-      <NavBar/>
-      <HighScores/>
-      <PlayGame/>
+    <div className={`flex flex-col w-screen backdrop-blur-sm fixed top-0 left-0 ${isPlaying ? '' : 'h-screen'}`}>
+      <NavBar isPlaying={isPlaying} onLogoClick={restartGame}/>
+      {!isPlaying && <HighScores />}
+      {!isPlaying && <PlayGameButton onPlayClick={toggleIsPlaying}/>}
     </div>
 
   );
